aeacb6f37d9eb0cc243802d52b308a9e0594093a
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:28:45 +0530

    LongestSubstring: Step 2 - Add brute force check all unique substrings baseline

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,11 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Problem 5: Longest Substring Without Repeating Characters
  * 
  * Given a string `s`, find the length of the longest substring without repeating characters.
  */
 public class LongestSubstring {
+
+    public static int lengthOfLongestSubstringBruteForce(String s) {
+        int maxLen = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (allUnique(s, i, j)) {
+                    maxLen = Math.max(maxLen, j - i);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    private static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            char ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+
     public static int lengthOfLongestSubstring(String s) {
-        // TODO: Implement solution
-        return 0;
+        return lengthOfLongestSubstringBruteForce(s);
     }
 }
