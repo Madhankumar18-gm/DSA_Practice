afcd91985c84c621c6368a7696d06a48fcdeeabe
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:58:06 +0530

    DecodeString: Step 2 - Add letter extraction baseline solution

--- DecodeString.java
+++ DecodeString.java
@@ -4,8 +4,17 @@
  * Given an encoded string, return its decoded string: k[encoded_string] repeated k times.
  */
 public class DecodeString {
+
+    // Simple single-bracket expansion placeholder
+    public static String decodeSimple(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetter(ch)) sb.append(ch);
+        }
+        return sb.toString();
+    }
+
     public static String decodeString(String s) {
-        // TODO: Implement string decoding
-        return "";
+        return decodeSimple(s);
     }
 }
