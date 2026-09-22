f968fc7cd2376a36ab624e50f52d2e722f352003
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:25:02 +0530

    ReverseLinkedList: Step 1 - Add ListNode class structure and method signature

---
+++ ReverseLinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * Problem 2: Reverse Linked List
+ * 
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+public class ReverseLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        // TODO: Implement reversal
+        return head;
+    }
+}
