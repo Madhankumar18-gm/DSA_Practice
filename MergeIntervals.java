0ef9504d6e5cf7ed0334a0eff04565c18836d1a5
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:05 +0530

    MergeIntervals: Step 2 - Add interval sorting baseline implementation

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
 /**
  * Problem 6: Merge Intervals
  * 
- * Given an array of `intervals` where intervals[i] = [starti, endi], merge all overlapping intervals,
- * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * Given an array of `intervals` where intervals[i] = [starti, endi], merge all overlapping intervals.
  */
 public class MergeIntervals {
+    
+    // Sort & simple linear pass merge
+    public static int[][] mergeBasic(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        return intervals;
+    }
+
     public static int[][] merge(int[][] intervals) {
-        // TODO: Implement interval merging
-        return new int[][]{};
+        return mergeBasic(intervals);
     }
 }
