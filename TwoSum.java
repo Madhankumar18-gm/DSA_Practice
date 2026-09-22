615dc2c2d97a3ae18653f3bcac20101122ec460f
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:24:48 +0530

    TwoSum: Step 4 - Implement optimized HashMap O(N) solution

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Problem 1: Two Sum
@@ -20,8 +22,21 @@ public class TwoSum {
         return new int[]{};
     }
 
+    // Optimized Approach: O(N) time complexity using HashMap
+    public static int[] twoSumOptimized(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
     public static int[] twoSum(int[] nums, int target) {
-        return twoSumBruteForce(nums, target);
+        return twoSumOptimized(nums, target);
     }
 
     public static void main(String[] args) {
