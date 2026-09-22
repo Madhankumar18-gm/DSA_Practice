934f49f4047590d95bf71ae8ea2215c080a0df33
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:56:58 +0530

    ImplementQueueUsingStacks: Step 1 - Add class structure and method signatures

---
+++ ImplementQueueUsingStacks.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+/**
+ * Problem 11: Implement Queue using Stacks
+ * 
+ * Implement a first-in first-out (FIFO) queue using only two stacks.
+ */
+public class ImplementQueueUsingStacks {
+    public static class MyQueue {
+        public MyQueue() { }
+        public void push(int x) { }
+        public int pop() { return 0; }
+        public int peek() { return 0; }
+        public boolean empty() { return true; }
+    }
+}
