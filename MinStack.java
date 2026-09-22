ded9aab3f2b2152d99a4d0b4810e4f3464ae43f7
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:38:08 +0530

    MinStack: Step 2 - Add naive O(N) getMin implementation

--- MinStack.java
+++ MinStack.java
@@ -3,15 +3,33 @@ import java.util.Stack;
 /**
  * Problem 8: Min Stack
  * 
- * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * Design a stack that supports push, pop, top, and retrieving the minimum element.
  */
 public class MinStack {
+    private Stack<Integer> stack;
+
     public MinStack() {
-        // TODO: Initialize data structures
+        stack = new Stack<>();
     }
     
-    public void push(int val) { }
-    public void pop() { }
-    public int top() { return 0; }
-    public int getMin() { return 0; }
+    public void push(int val) {
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if (!stack.isEmpty()) stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    // Naive O(N) scan for minimum element
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+        for (int val : stack) {
+            min = Math.min(min, val);
+        }
+        return min;
+    }
 }
