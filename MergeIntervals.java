688aa2caba021da0883b417c61df3b9a0c87a938
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:03 +0530

    MergeIntervals: Step 1 - Add class structure and method skeleton

---
+++ MergeIntervals.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 6: Merge Intervals
+ * 
+ * Given an array of `intervals` where intervals[i] = [starti, endi], merge all overlapping intervals,
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        // TODO: Implement interval merging
+        return new int[][]{};
+    }
+}
