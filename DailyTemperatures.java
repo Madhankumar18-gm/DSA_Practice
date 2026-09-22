24ef08a6e353e20fe744eda3a3cf490b8994c19c
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:56:38 +0530

    DailyTemperatures: Step 1 - Add class structure and method signature

---
+++ DailyTemperatures.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 10: Daily Temperatures
+ * 
+ * Given an array of integers `temperatures` represents the daily temperatures, return an array `answer`
+ * such that answer[i] is the number of days you have to wait after the i-th day to get a warmer temperature.
+ */
+public class DailyTemperatures {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        // TODO: Implement solution
+        return new int[]{};
+    }
+}
