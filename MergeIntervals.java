b1d9bbbe92fa592bf434cdf78b90035367765ae7
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:08 +0530

    MergeIntervals: Step 3 - Add main test runner for sorted interval baseline

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -6,8 +6,7 @@ import java.util.Arrays;
  * Given an array of `intervals` where intervals[i] = [starti, endi], merge all overlapping intervals.
  */
 public class MergeIntervals {
-    
-    // Sort & simple linear pass merge
+
     public static int[][] mergeBasic(int[][] intervals) {
         if (intervals.length <= 1) return intervals;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
@@ -16,5 +15,10 @@ public class MergeIntervals {
 
     public static int[][] merge(int[][] intervals) {
         return mergeBasic(intervals);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        System.out.println("Sorted Intervals: " + Arrays.deepToString(merge(intervals)));
     }
 }
