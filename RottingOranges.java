cd1c67a3c9fabe4dcf84166c9713da6e984c6119
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:47 +0530

    RottingOranges: Step 1 - Add class structure and method signature

---
+++ RottingOranges.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 14: Rotting Oranges
+ * 
+ * You are given an m x n grid where each cell can have 0 (empty), 1 (fresh orange), or 2 (rotten orange).
+ * Return the minimum number of minutes that must elapse until no cell has a fresh orange. If impossible, return -1.
+ */
+public class RottingOranges {
+    public static int orangesRotting(int[][] grid) {
+        // TODO: Implement BFS queue
+        return -1;
+    }
+}
