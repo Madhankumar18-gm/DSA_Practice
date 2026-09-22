75906d75275567d97776e078a80991255d8db19e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:56:43 +0530

    DailyTemperatures: Step 2 - Add brute force O(N^2) baseline solution

--- DailyTemperatures.java
+++ DailyTemperatures.java
@@ -1,12 +1,28 @@
+import java.util.Arrays;
+
 /**
  * Problem 10: Daily Temperatures
  * 
- * Given an array of integers `temperatures` represents the daily temperatures, return an array `answer`
- * such that answer[i] is the number of days you have to wait after the i-th day to get a warmer temperature.
+ * Given an array of integers `temperatures`, return an array `answer` such that answer[i] is the number of days to wait.
  */
 public class DailyTemperatures {
+
+    // Brute Force O(N^2) time complexity
+    public static int[] dailyTemperaturesBruteForce(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    result[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
     public static int[] dailyTemperatures(int[] temperatures) {
-        // TODO: Implement solution
-        return new int[]{};
+        return dailyTemperaturesBruteForce(temperatures);
     }
 }
