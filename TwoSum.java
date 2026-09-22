d6bed30bb4a65cd03fe2204adbdba3cf9d7eb0a0
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:24:40 +0530

    TwoSum: Step 1 - Add problem statement and class skeleton

---
+++ TwoSum.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 1: Two Sum
+ * 
+ * Given an array of integers `nums` and an integer `target`, return indices of 
+ * the two numbers such that they add up to target.
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        // TODO: Implement solution
+        return new int[]{};
+    }
+}
