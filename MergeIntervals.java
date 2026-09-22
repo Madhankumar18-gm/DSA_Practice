6b567ef679e29de4705162d27f3d7c2b7c91a75e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:10 +0530

    MergeIntervals: Step 4 - Implement optimal Sorting + Greedy Merging algorithm

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Problem 6: Merge Intervals
@@ -7,18 +9,36 @@ import java.util.Arrays;
  */
 public class MergeIntervals {
 
-    public static int[][] mergeBasic(int[][] intervals) {
+    // Optimal Sorting + Greedy Merging: O(N log N)
+    public static int[][] mergeOptimal(int[][] intervals) {
         if (intervals.length <= 1) return intervals;
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        return intervals;
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        merged.add(currentInterval);
+
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+
+            if (currentEnd >= nextStart) {
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                currentInterval = interval;
+                merged.add(currentInterval);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
     }
 
     public static int[][] merge(int[][] intervals) {
-        return mergeBasic(intervals);
+        return mergeOptimal(intervals);
     }
 
     public static void main(String[] args) {
         int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        System.out.println("Sorted Intervals: " + Arrays.deepToString(merge(intervals)));
+        System.out.println("Merged Intervals: " + Arrays.deepToString(merge(intervals)));
     }
 }
