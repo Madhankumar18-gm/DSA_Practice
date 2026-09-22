6c134c81062a4bb883443220471dacb9538cd160
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:24 +0530

    CoinChange: Step 1 - Add class structure and method signature

---
+++ CoinChange.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 7: Coin Change
+ * 
+ * Given an integer array `coins` representing coins of different denominations and an integer `amount`,
+ * return the fewest number of coins that you need to make up that amount.
+ */
+public class CoinChange {
+    public static int coinChange(int[] coins, int amount) {
+        // TODO: Implement DP solution
+        return -1;
+    }
+}
