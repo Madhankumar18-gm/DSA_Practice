f71cee6abe949eed6d1c64ca01162495c334e23e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:24:43 +0530

    TwoSum: Step 2 - Add brute force implementation

--- TwoSum.java
+++ TwoSum.java
@@ -5,8 +5,20 @@
  * the two numbers such that they add up to target.
  */
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
-        // TODO: Implement solution
+
+    // Brute Force Approach: O(N^2) time complexity
+    public static int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
         return new int[]{};
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        return twoSumBruteForce(nums, target);
     }
 }
