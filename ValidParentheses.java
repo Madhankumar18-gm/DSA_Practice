e48916200c78b9b5a3c2efe1155ec399af0ab6cc
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:28:04 +0530

    ValidParentheses: Step 2 - Add string replacement naive baseline solution

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -5,8 +5,14 @@
  * determine if the input string is valid.
  */
 public class ValidParentheses {
+    public static boolean isValidNaive(String s) {
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        }
+        return s.isEmpty();
+    }
+
     public static boolean isValid(String s) {
-        // TODO: Implement stack check
-        return false;
+        return isValidNaive(s);
     }
 }
