391f5a7bd263d1cdfff92fd8bf3727515f0cef6a
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:58:24 +0530

    AsteroidCollision: Step 1 - Add class structure and method signature

---
+++ AsteroidCollision.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 16: Asteroid Collision
+ * 
+ * Find out the state of the asteroids after all collisions.
+ */
+public class AsteroidCollision {
+    public static int[] asteroidCollision(int[] asteroids) {
+        // TODO: Implement collision simulation
+        return new int[]{};
+    }
+}
