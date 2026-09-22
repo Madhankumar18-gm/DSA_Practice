bae7687d6c59444a7efd33aef068da173395940e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:53 +0530

    RottingOranges: Step 4 - Implement optimal Multi-Source Queue BFS algorithm

--- RottingOranges.java
+++ RottingOranges.java
@@ -8,7 +8,8 @@ import java.util.Queue;
  */
 public class RottingOranges {
 
-    public static int orangesRotting(int[][] grid) {
+    // Multi-source BFS Queue O(R * C) solution
+    public static int orangesRottingBFS(int[][] grid) {
         if (grid == null || grid.length == 0) return 0;
         int rows = grid.length, cols = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
@@ -16,18 +17,42 @@ public class RottingOranges {
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == 2) {
-                    queue.add(new int[]{r, c});
-                } else if (grid[r][c] == 1) {
-                    freshCount++;
+                if (grid[r][c] == 2) queue.add(new int[]{r, c});
+                else if (grid[r][c] == 1) freshCount++;
+            }
+        }
+
+        if (freshCount == 0) return 0;
+
+        int minutes = 0;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!queue.isEmpty() && freshCount > 0) {
+            minutes++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                for (int[] d : dirs) {
+                    int nr = curr[0] + d[0];
+                    int nc = curr[1] + d[1];
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        freshCount--;
+                        queue.add(new int[]{nr, nc});
+                    }
                 }
             }
         }
-        return freshCount == 0 ? 0 : -1;
+
+        return freshCount == 0 ? minutes : -1;
+    }
+
+    public static int orangesRotting(int[][] grid) {
+        return orangesRottingBFS(grid);
     }
 
     public static void main(String[] args) {
-        int[][] grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 2}};
-        System.out.println("Result: " + orangesRotting(grid));
+        int[][] grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+        System.out.println("Minutes to Rot All: " + orangesRotting(grid));
     }
 }
