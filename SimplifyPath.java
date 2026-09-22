974a1a617b4fa4147e5990db2176bcd61b2ba1b5
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:58:44 +0530

    SimplifyPath: Step 1 - Add class structure and method signature

---
+++ SimplifyPath.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 17: Simplify Path
+ * 
+ * Given an absolute path for a Unix-style file system, transform it to the simplified canonical path.
+ */
+public class SimplifyPath {
+    public static String simplifyPath(String path) {
+        // TODO: Implement path canonicalization
+        return "/";
+    }
+}
