4e41ca56ea88eba2a5ce85db8c0274ee22b7b4d2
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:38:27 +0530

    EvaluateReversePolishNotation: Step 1 - Add class skeleton and method signature

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 9: Evaluate Reverse Polish Notation
+ * 
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation (RPN).
+ */
+public class EvaluateReversePolishNotation {
+    public static int evalRPN(String[] tokens) {
+        // TODO: Implement evaluation
+        return 0;
+    }
+}
