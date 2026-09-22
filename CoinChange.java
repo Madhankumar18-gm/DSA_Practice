702035483031de3c856d48358c89e2b5b5c6592f
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:29:32 +0530

    CoinChange: Step 4 - Implement optimal 1D Bottom-Up Dynamic Programming approach

--- CoinChange.java
+++ CoinChange.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Problem 7: Coin Change
  * 
@@ -6,22 +8,25 @@
  */
 public class CoinChange {
 
-    public static int coinChangeRecursive(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
+    // Optimal 1D Dynamic Programming O(amount * N)
+    public static int coinChangeDP(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
 
-        int minCoins = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int subRes = coinChangeRecursive(coins, amount - coin);
-            if (subRes >= 0 && subRes < minCoins) {
-                minCoins = subRes + 1;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
-        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
     public static int coinChange(int[] coins, int amount) {
-        return coinChangeRecursive(coins, amount);
+        return coinChangeDP(coins, amount);
     }
 
     public static void main(String[] args) {
