fb7ee27ddc4051730bf467eb627d50deb3c22c20
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:31 +0530

    SlidingWindowMaximum: Step 1 - Add class structure and method signature

---
+++ SlidingWindowMaximum.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 13: Sliding Window Maximum
+ * 
+ * You are given an array of integers `nums`, there is a sliding window of size `k` which is moving from the
+ * very left of the array to the very right. Return the max sliding window.
+ */
+public class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        // TODO: Implement window max
+        return new int[]{};
+    }
+}
