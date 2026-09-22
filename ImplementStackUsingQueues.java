93416162df7ce74844e13020ccbd5c1e1220f62c
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:15 +0530

    ImplementStackUsingQueues: Step 1 - Add class structure and method signatures

---
+++ ImplementStackUsingQueues.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Problem 12: Implement Stack using Queues
+ * 
+ * Implement a last-in-first-out (LIFO) stack using only standard queues.
+ */
+public class ImplementStackUsingQueues {
+    public static class MyStack {
+        public MyStack() { }
+        public void push(int x) { }
+        public int pop() { return 0; }
+        public int top() { return 0; }
+        public boolean empty() { return true; }
+    }
+}
