3c15ff9d0c61d35324defff2300d6dd1a458a49e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:58:03 +0530

    DecodeString: Step 1 - Add class structure and method signature

---
+++ DecodeString.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 15: Decode String
+ * 
+ * Given an encoded string, return its decoded string: k[encoded_string] repeated k times.
+ */
+public class DecodeString {
+    public static String decodeString(String s) {
+        // TODO: Implement string decoding
+        return "";
+    }
+}
