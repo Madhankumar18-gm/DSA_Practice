88bc91289c594264ea6f4aae9be4e5c78f57bd53
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:28:42 +0530

    LongestSubstring: Step 1 - Add class structure and method signature

---
+++ LongestSubstring.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 5: Longest Substring Without Repeating Characters
+ * 
+ * Given a string `s`, find the length of the longest substring without repeating characters.
+ */
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        // TODO: Implement solution
+        return 0;
+    }
+}
