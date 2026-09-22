7af9aa895a03d3db764ef9f5a92eb862ad3f6b2d
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:28:01 +0530

    ValidParentheses: Step 1 - Add class structure and method signature

---
+++ ValidParentheses.java
@@ -0,0 +1,12 @@
+/**
+ * Problem 3: Valid Parentheses
+ * 
+ * Given a string `s` containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ */
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        // TODO: Implement stack check
+        return false;
+    }
+}
