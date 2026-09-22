b7c1775efed56acc4e3227c9b869e88b057d8b41
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:58:31 +0530

    AsteroidCollision: Step 4 - Implement optimal Stack O(N) collision algorithm

--- AsteroidCollision.java
+++ AsteroidCollision.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Problem 16: Asteroid Collision
@@ -7,12 +8,34 @@ import java.util.Arrays;
  */
 public class AsteroidCollision {
 
-    public static int[] asteroidCollisionNaive(int[] asteroids) {
-        return asteroids;
+    // Optimal Stack O(N) simulation
+    public static int[] asteroidCollisionStack(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int a : asteroids) {
+            boolean exploded = false;
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                if (stack.peek() < -a) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == -a) {
+                    stack.pop();
+                }
+                exploded = true;
+                break;
+            }
+            if (!exploded) {
+                stack.push(a);
+            }
+        }
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
     }
 
     public static int[] asteroidCollision(int[] asteroids) {
-        return asteroidCollisionNaive(asteroids);
+        return asteroidCollisionStack(asteroids);
     }
 
     public static void main(String[] args) {
