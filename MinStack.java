825cff43a1f3a5270f461aaea2ea6900fa1c2c1c
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 19:38:06 +0530

    MinStack: Step 1 - Add class structure and method signatures

---
+++ MinStack.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+/**
+ * Problem 8: Min Stack
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ */
+public class MinStack {
+    public MinStack() {
+        // TODO: Initialize data structures
+    }
+    
+    public void push(int val) { }
+    public void pop() { }
+    public int top() { return 0; }
+    public int getMin() { return 0; }
+}
