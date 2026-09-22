4cb5e627b931a0caaec55514bc4bc08ca61bc35b
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:33 +0530

    SlidingWindowMaximum: Step 2 - Add brute force O(N * k) baseline solution

--- SlidingWindowMaximum.java
+++ SlidingWindowMaximum.java
@@ -1,12 +1,27 @@
+import java.util.Arrays;
+
 /**
  * Problem 13: Sliding Window Maximum
  * 
- * You are given an array of integers `nums`, there is a sliding window of size `k` which is moving from the
- * very left of the array to the very right. Return the max sliding window.
+ * You are given an array of integers `nums`, there is a sliding window of size `k`.
  */
 public class SlidingWindowMaximum {
+
+    // Brute Force O(N * k) solution
+    public static int[] maxSlidingWindowBrute(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        for (int i = 0; i <= n - k; i++) {
+            int max = nums[i];
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        // TODO: Implement window max
-        return new int[]{};
+        return maxSlidingWindowBrute(nums, k);
     }
 }
