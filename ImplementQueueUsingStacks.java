a1389953d46440606035af999bd8a013db1a72b3
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-22 21:57:05 +0530

    ImplementQueueUsingStacks: Step 4 - Implement optimal amortized O(1) two-stack queue

--- ImplementQueueUsingStacks.java
+++ ImplementQueueUsingStacks.java
@@ -3,35 +3,38 @@ import java.util.Stack;
 /**
  * Problem 11: Implement Queue using Stacks
  * 
- * Implement a first-in first-out (FIFO) queue using two stacks.
+ * Implement a first-in first-out (FIFO) queue using two stacks (inStack & outStack).
  */
 public class ImplementQueueUsingStacks {
     public static class MyQueue {
-        private Stack<Integer> s1 = new Stack<>();
-        private Stack<Integer> s2 = new Stack<>();
+        private Stack<Integer> inStack = new Stack<>();
+        private Stack<Integer> outStack = new Stack<>();
 
         public MyQueue() { }
 
+        // Amortized O(1) Push
         public void push(int x) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
-            }
-            s1.push(x);
-            while (!s2.isEmpty()) {
-                s1.push(s2.pop());
-            }
+            inStack.push(x);
         }
 
+        // Amortized O(1) Pop
         public int pop() {
-            return s1.pop();
+            peek();
+            return outStack.pop();
         }
 
+        // Amortized O(1) Peek
         public int peek() {
-            return s1.peek();
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.peek();
         }
 
         public boolean empty() {
-            return s1.isEmpty();
+            return inStack.isEmpty() && outStack.isEmpty();
         }
     }
 
@@ -39,6 +42,6 @@ public class ImplementQueueUsingStacks {
         MyQueue queue = new MyQueue();
         queue.push(1);
         queue.push(2);
-        System.out.println("Peek: " + queue.peek()); // 1
+        System.out.println("Peek: " + queue.peek());
     }
 }
