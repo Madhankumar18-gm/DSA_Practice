15ebad58a363039436643576f0f30b5c02c642ca
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:00:21 +0530

    Permutations: Step 1 - Add class structure and method signature

---
+++ Permutations.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 20: Permutations
+ * 
+ * Given an array `nums` of distinct integers, return all the possible permutations.
+ */
+public class Permutations {
+    public static List<List<Integer>> permute(int[] nums) {
+        // TODO: Implement permutations
+        return new ArrayList<>();
+    }
+}
