d44bed4937f0d67ec614be2c1b4ccf0277b88f98
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:01:58 +0530

    WordSearch: Step 2 - Add grid traversal DFS helper baseline

--- WordSearch.java
+++ WordSearch.java
@@ -4,8 +4,16 @@
  * Given an m x n grid of characters `board` and a string `word`, return true if word exists in the grid.
  */
 public class WordSearch {
+
+    public static boolean dfsBasic(char[][] board, String word, int r, int c, int index) {
+        if (index == word.length()) return true;
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+        return false;
+    }
+
     public static boolean exist(char[][] board, String word) {
-        // TODO: Implement grid word search
         return false;
     }
 }
