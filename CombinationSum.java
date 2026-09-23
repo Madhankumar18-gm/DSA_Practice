a602db75feb69c014fcde6438e233eecb8be3afd
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:01:31 +0530

    CombinationSum: Step 1 - Add class structure and method signature

---
+++ CombinationSum.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 23: Combination Sum
+ * 
+ * Given an array of distinct integers `candidates` and a `target`, return all unique combinations summing to target.
+ */
+public class CombinationSum {
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // TODO: Implement solution
+        return new ArrayList<>();
+    }
+}
