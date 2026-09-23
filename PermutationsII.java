8ce87e4e1c3396421a4c8372e991fdc48a9b5a0d
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:00:44 +0530

    PermutationsII: Step 1 - Add class structure and method signature

---
+++ PermutationsII.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 21: Permutations II
+ * 
+ * Given a collection of numbers, `nums`, that might contain duplicates, return all possible unique permutations.
+ */
+public class PermutationsII {
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        // TODO: Implement unique permutations
+        return new ArrayList<>();
+    }
+}
