e25ac988e1046296ba1b2b55561b6f4d2a1ca776
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:00:11 +0530

    SudokuSolver: Step 6 - Add standard 9x9 Sudoku grid test case

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -38,16 +38,19 @@ public class SudokuSolver {
         }
     }
 
-    public static void printBoard(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println("Sudoku Visualizer Ready.");
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        solveSudoku(board);
+        System.out.println("Solved First Cell: " + board[0][2]);
     }
 }
