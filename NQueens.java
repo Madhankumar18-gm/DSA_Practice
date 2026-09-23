1a89713be2c761a50c873fdeff33350444b649d1
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 08:59:34 +0530

    NQueens: Step 1 - Add class structure and method signature

---
+++ NQueens.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 18: N-Queens
+ * 
+ * Place n queens on an n x n chessboard such that no two queens attack each other.
+ */
+public class NQueens {
+    public static List<List<String>> solveNQueens(int n) {
+        // TODO: Implement solution
+        return new ArrayList<>();
+    }
+}
