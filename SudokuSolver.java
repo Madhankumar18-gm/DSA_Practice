0cb189229211347896dd0ab557adcef0ebd2a2fc
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 08:59:59 +0530

    SudokuSolver: Step 1 - Add class structure and method signature

---
+++ SudokuSolver.java
@@ -0,0 +1,10 @@
+/**
+ * Problem 19: Sudoku Solver
+ * 
+ * Write a program to solve a Sudoku puzzle by filling the empty cells in-place.
+ */
+public class SudokuSolver {
+    public static void solveSudoku(char[][] board) {
+        // TODO: Implement solver
+    }
+}
