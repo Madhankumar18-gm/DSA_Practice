2292273397464a795643886217784a72f7ca84ca
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:00:47 +0530

    PermutationsII: Step 2 - Add HashSet deduplication baseline structure

--- PermutationsII.java
+++ PermutationsII.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Problem 21: Permutations II
@@ -7,8 +9,15 @@ import java.util.List;
  * Given a collection of numbers, `nums`, that might contain duplicates, return all possible unique permutations.
  */
 public class PermutationsII {
+
+    // Naive HashSet deduplication
+    public static List<List<Integer>> permuteUniqueNaive(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        // Deduplication container
+        return new ArrayList<>(set);
+    }
+
     public static List<List<Integer>> permuteUnique(int[] nums) {
-        // TODO: Implement unique permutations
-        return new ArrayList<>();
+        return permuteUniqueNaive(nums);
     }
 }
