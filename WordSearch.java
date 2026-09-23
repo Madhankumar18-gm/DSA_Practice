3f86d828126a454aecae376b8d562cd69079f7db
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:01:55 +0530

    WordSearch: Step 1 - Add class structure and method signature

---
+++ WordSearch.java
@@ -0,0 +1,11 @@
+/**
+ * Problem 24: Word Search
+ * 
+ * Given an m x n grid of characters `board` and a string `word`, return true if word exists in the grid.
+ */
+public class WordSearch {
+    public static boolean exist(char[][] board, String word) {
+        // TODO: Implement grid word search
+        return false;
+    }
+}
