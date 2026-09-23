6d4e332a98bce4d3a71af9b3036b93a199ab9d88
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:02:17 +0530

    PalindromePartitioning: Step 1 - Add class structure and method signature

---
+++ PalindromePartitioning.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 25: Palindrome Partitioning
+ * 
+ * Given a string `s`, partition `s` such that every substring of the partition is a palindrome.
+ */
+public class PalindromePartitioning {
+    public static List<List<String>> partition(String s) {
+        // TODO: Implement palindrome partitioning
+        return new ArrayList<>();
+    }
+}
