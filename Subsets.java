867828b8a97800c3b475904de295b3a89103e105
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-23 09:01:08 +0530

    Subsets: Step 1 - Add class structure and method signature

---
+++ Subsets.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem 22: Subsets
+ * 
+ * Given an integer array `nums` of unique elements, return all possible subsets (the power set).
+ */
+public class Subsets {
+    public static List<List<Integer>> subsets(int[] nums) {
+        // TODO: Implement subsets
+        return new ArrayList<>();
+    }
+}
