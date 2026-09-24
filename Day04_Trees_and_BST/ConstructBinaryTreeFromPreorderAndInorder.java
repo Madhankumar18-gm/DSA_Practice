50e82a6e712e46539b176043cbdac71edcc01149
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-24 08:38:57 +0530

    ConstructBinaryTreeFromPreorderAndInorder: Step 2 - Add recursive tree builder baseline

--- Day04_Trees_and_BST/ConstructBinaryTreeFromPreorderAndInorder.java
+++ Day04_Trees_and_BST/ConstructBinaryTreeFromPreorderAndInorder.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Problem 32: Construct Binary Tree from Preorder and Inorder Traversal
  * 
@@ -16,8 +19,20 @@ public class ConstructBinaryTreeFromPreorderAndInorder {
         }
     }
 
+    // Baseline recursive tree builder
+    private static TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) inIndex = i;
+        }
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        return root;
+    }
+
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        // TODO: Implement tree construction
-        return null;
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
     }
 }
