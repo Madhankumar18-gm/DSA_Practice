1dcc3fa8cba180acba1867d5ab6bdf2f9d67d4f6
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-24 08:36:55 +0530

    ValidateBinarySearchTree: Step 1 - Add TreeNode class and method signature

---
+++ Day04_Trees_and_BST/ValidateBinarySearchTree.java
@@ -0,0 +1,23 @@
+/**
+ * Problem 27: Validate Binary Search Tree
+ * 
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ */
+public class ValidateBinarySearchTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        // TODO: Implement BST validation
+        return false;
+    }
+}
