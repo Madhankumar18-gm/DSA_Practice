556ce7119b87016c3478d6b576fa62a4a659e17b
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-24 08:37:48 +0530

    LowestCommonAncestorBST: Step 4 - Implement optimal Iterative O(H) BST search algorithm

--- Day04_Trees_and_BST/LowestCommonAncestorBST.java
+++ Day04_Trees_and_BST/LowestCommonAncestorBST.java
@@ -16,14 +16,24 @@ public class LowestCommonAncestorBST {
         }
     }
 
-    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    // Optimal Iterative O(H) time, O(1) space solution
+    public static TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || p == null || q == null) return null;
-        if (p.val < root.val && q.val < root.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else if (p.val > root.val && q.val > root.val) {
-            return lowestCommonAncestor(root.right, p, q);
+        TreeNode curr = root;
+        while (curr != null) {
+            if (p.val < curr.val && q.val < curr.val) {
+                curr = curr.left;
+            } else if (p.val > curr.val && q.val > curr.val) {
+                curr = curr.right;
+            } else {
+                return curr;
+            }
         }
-        return root;
+        return null;
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return lowestCommonAncestorIterative(root, p, q);
     }
 
     public static void main(String[] args) {
