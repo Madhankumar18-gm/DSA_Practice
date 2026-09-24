a1a699918dc5450f90a3198f6f54270781eff3f2
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-24 08:36:30 +0530

    MaximumDepthOfBinaryTree: Step 1 - Add TreeNode class and method signature

---
+++ Day04_Trees_and_BST/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Problem 26: Maximum Depth of Binary Tree
+ * 
+ * Given the root of a binary tree, return its maximum depth.
+ */
+public class MaximumDepthOfBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        // TODO: Implement max depth
+        return 0;
+    }
+}
