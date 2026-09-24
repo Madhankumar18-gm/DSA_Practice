26bf726255cfba8f65d11208950c2364feecf216
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-24 08:37:19 +0530

    InvertBinaryTree: Step 1 - Add TreeNode class and method signature

---
+++ Day04_Trees_and_BST/InvertBinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Problem 28: Invert Binary Tree
+ * 
+ * Given the root of a binary tree, invert the tree, and return its root.
+ */
+public class InvertBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        // TODO: Implement inversion
+        return root;
+    }
+}
