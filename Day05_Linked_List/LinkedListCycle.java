a05dbe58a109c80477dd6ae5f239e42e4976e2f8
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:07:35 +0530

    LinkedListCycle: Add Javadoc overview and space complexity analysis

--- Day05_Linked_List/LinkedListCycle.java
+++ Day05_Linked_List/LinkedListCycle.java
@@ -1,3 +1,11 @@
+/**
+ * Problem 37: Linked List Cycle (LeetCode 141)
+ * 
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * 
+ * Time Complexity: O(N) where N is the number of nodes.
+ * Space Complexity: O(1) memory using Floyd's Tortoise and Hare algorithm.
+ */
 public class LinkedListCycle {
     public static class ListNode {
         int val;
