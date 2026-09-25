ceac326816471a6653a60ef0408115c96626c29f
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:09:01 +0530

    MergeTwoSortedLists: Add Javadoc documentation & complexity analysis

--- Day05_Linked_List/MergeTwoSortedLists.java
+++ Day05_Linked_List/MergeTwoSortedLists.java
@@ -1,3 +1,11 @@
+/**
+ * Problem 39: Merge Two Sorted Lists (LeetCode 21)
+ * 
+ * Merge two sorted linked lists and return it as a sorted list.
+ * 
+ * Time Complexity: O(N + M) where N and M are the lengths of list1 and list2.
+ * Space Complexity: O(1) auxiliary space (iterative splicing).
+ */
 public class MergeTwoSortedLists {
     public static class ListNode {
         int val;
