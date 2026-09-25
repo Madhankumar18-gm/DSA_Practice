0c8a6353e6d7fd9b38385851ae51ec0115a57754
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:10:14 +0530

    RemoveNthNodeFromEnd: Add Javadoc documentation & complexity analysis

--- Day05_Linked_List/RemoveNthNodeFromEnd.java
+++ Day05_Linked_List/RemoveNthNodeFromEnd.java
@@ -1,3 +1,11 @@
+/**
+ * Problem 40: Remove Nth Node From End of List (LeetCode 19)
+ * 
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ * 
+ * Time Complexity: O(N) single-pass.
+ * Space Complexity: O(1) using two pointers with an n-node gap.
+ */
 public class RemoveNthNodeFromEnd {
     public static class ListNode {
         int val;
