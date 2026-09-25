c2bf36a7388e44c54d24c409f8f8f9460a85aeae
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:09:06 +0530

    MergeTwoSortedLists: Implement iterative dummy pointer merging logic

--- Day05_Linked_List/MergeTwoSortedLists.java
+++ Day05_Linked_List/MergeTwoSortedLists.java
@@ -38,14 +38,29 @@ public class MergeTwoSortedLists {
     }
 
     /**
-     * Merges two sorted linked lists.
+     * Merges two sorted linked lists using dummy head pointer manipulation.
      * @param list1 First sorted list
      * @param list2 Second sorted list
      * @return Head of merged sorted list
      */
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-        return null;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) curr.next = list1;
+        if (list2 != null) curr.next = list2;
+
+        return dummy.next;
     }
 }
