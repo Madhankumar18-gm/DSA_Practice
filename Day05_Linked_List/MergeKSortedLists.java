7750b75b66580c3b70e92e02a7adb33b3d8f2353
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:13:50 +0530

    MergeKSortedLists: Add class skeleton & PriorityQueue import

---
+++ Day05_Linked_List/MergeKSortedLists.java
@@ -0,0 +1,10 @@
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
