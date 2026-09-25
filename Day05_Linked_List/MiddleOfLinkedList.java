73d6ac0fd56f886dfa56ddecc2ce7f035446bc4e
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:06:39 +0530

    MiddleOfLinkedList: Add Javadoc documentation & complexity analysis

--- Day05_Linked_List/MiddleOfLinkedList.java
+++ Day05_Linked_List/MiddleOfLinkedList.java
@@ -1,3 +1,12 @@
+/**
+ * Problem 36: Middle of the Linked List (LeetCode 876)
+ * 
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * Time Complexity: O(N) where N is the number of nodes.
+ * Space Complexity: O(1) auxiliary space.
+ */
 public class MiddleOfLinkedList {
     public static class ListNode {
         int val;
