67e814418e701b46c838f2d4a10bd8c075b6d4f8
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-25 18:09:03 +0530

    MergeTwoSortedLists: Add buildList & toListString helper methods

--- Day05_Linked_List/MergeTwoSortedLists.java
+++ Day05_Linked_List/MergeTwoSortedLists.java
@@ -13,4 +13,27 @@ public class MergeTwoSortedLists {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+
+    public static ListNode buildList(int[] values) {
+        if (values == null || values.length == 0) return null;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        for (int v : values) {
+            curr.next = new ListNode(v);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    public static String toListString(ListNode head) {
+        StringBuilder sb = new StringBuilder("[");
+        ListNode curr = head;
+        while (curr != null) {
+            sb.append(curr.val);
+            if (curr.next != null) sb.append(", ");
+            curr = curr.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
