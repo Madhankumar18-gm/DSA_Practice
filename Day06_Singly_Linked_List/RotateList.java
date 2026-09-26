c9e2e8b6dc20cd15510a7e025b384d0d1a7742bb
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-26 20:21:39 +0530

    RotateList: Add class skeleton & ListNode definition

---
+++ Day06_Singly_Linked_List/RotateList.java
@@ -0,0 +1,8 @@
+public class RotateList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
