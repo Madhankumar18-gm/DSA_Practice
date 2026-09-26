e625b9d0b9db2c5b0767543d8693bac93084af61
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-26 20:23:12 +0530

    ReverseNodesInKGroup: Add Javadoc overview & space/time complexity bounds

--- Day06_Singly_Linked_List/ReverseNodesInKGroup.java
+++ Day06_Singly_Linked_List/ReverseNodesInKGroup.java
@@ -1,3 +1,13 @@
+/**
+ * Problem 53: Reverse Nodes in k-Group (LeetCode 25)
+ * 
+ * Given the head of a linked list, reverse the nodes of a list k at a time, and return its modified list.
+ * k is a positive integer and is less than or equal to the length of the linked list.
+ * If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1) auxiliary space (Hard difficulty linked list problem).
+ */
 public class ReverseNodesInKGroup {
     public static class ListNode {
         int val;
