3986be5078bacbcf808345f8b1b463bfacff32a6
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-26 20:22:48 +0530

    SwapNodesInPairs: Add Javadoc overview & complexity bounds

--- Day06_Singly_Linked_List/SwapNodesInPairs.java
+++ Day06_Singly_Linked_List/SwapNodesInPairs.java
@@ -1,3 +1,12 @@
+/**
+ * Problem 52: Swap Nodes in Pairs (LeetCode 24)
+ * 
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed).
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1) auxiliary space (iterative dummy node manipulation).
+ */
 public class SwapNodesInPairs {
     public static class ListNode {
         int val;
