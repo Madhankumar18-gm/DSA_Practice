de222b392b41a36a25ce6c200a7f7afbd1a75ce3
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-26 20:18:59 +0530

    PalindromeLinkedList: Add Javadoc algorithm description & complexity analysis

--- Day06_Singly_Linked_List/PalindromeLinkedList.java
+++ Day06_Singly_Linked_List/PalindromeLinkedList.java
@@ -1,3 +1,17 @@
+/**
+ * Problem 45: Palindrome Linked List (LeetCode 234)
+ * 
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+ * 
+ * Algorithm:
+ * 1. Find middle of linked list using fast & slow pointers.
+ * 2. Reverse the second half of the linked list.
+ * 3. Compare values of first half and reversed second half.
+ * 4. Restore list structure (optional) and return result.
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1) in-place modification.
+ */
 public class PalindromeLinkedList {
     public static class ListNode {
         int val;
