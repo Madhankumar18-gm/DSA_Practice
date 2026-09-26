69b51310ef6ff0eb5fae834af6bbf410ef1eb054
Madhan Kumar <gmadhankumar50@gmail.com>
2026-09-26 20:21:18 +0530

    PartitionList: Add Javadoc overview & complexity analysis

--- Day06_Singly_Linked_List/PartitionList.java
+++ Day06_Singly_Linked_List/PartitionList.java
@@ -1,3 +1,12 @@
+/**
+ * Problem 49: Partition List (LeetCode 86)
+ * 
+ * Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ * Preserve original relative order of nodes in each of the two partitions.
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1) auxiliary space (using two dummy buckets).
+ */
 public class PartitionList {
     public static class ListNode {
         int val;
